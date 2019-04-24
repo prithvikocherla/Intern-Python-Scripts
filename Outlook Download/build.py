9df13fe49fcb0c9f040a9987d2a2f4a38fd70109
prithvikocherla <prithvikocherla@github.com>
2019-04-24 13:51:11 -0400

    Large file Upload

--- Outlook Download/build.py
+++ Outlook Download/build.py
@@ -1,8 +1,3 @@
-import os
-
-os.system("pyinstaller --log-level=WARN \
-    --onefile \
-    --console \
-    --icon=icon='icon\daily-stats.ico'\
-    --clean\
-    load.spec")
+version https://git-lfs.github.com/spec/v1
+oid sha256:03ea7d6225eb05935cc3e892a257449ee2f1f610559f8007ef4539a45a08f2ae
+size 160
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Intern-Python-Scripts
-It contains some of the python scripts that I developed during my Internship at Bristol-Myers Squibb
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b1a909235b1c8870703ec91f4b8ca33b7ea6bfc3801c5dfe02f70ed5d25d237
+size 127
